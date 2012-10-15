366f7e0ff80c427efb8243c7c4103deb2e9432bd
Simon Oram <simon@electrosoup.co.uk>
2012-10-15 15:36:51 +0100

    added tests

--- deform_wizard/tests.py
+++ deform_wizard/tests.py
@@ -1,64 +1,172 @@
 import unittest
-from mock import patch
-from mock import Mock
+
 from pyramid import testing
-from pyramid.exceptions import ConfigurationError
+
+from mock import Mock
 
 
 class Wizard(unittest.TestCase):
 
     def setUp(self):
 
+        from pyramid.testing import DummyRequest
+
+        self.request = DummyRequest()
+
         self.config = testing.setUp()
+
+        self.request = DummyRequest()
 
     def tearDown(self):
 
         testing.tearDown()
 
-    def test_init_wizrd(self):
+    def get_csrf_request(self, post=None):
+
+        csrf = 'abc'
+
+        if not u'csrf_token' in post.keys():
+
+            post.update({
+
+                'csrf_token': csrf
+
+            })
+
+        request = testing.DummyRequest(post)
+
+        request.session = testing.DummySession()
+
+        csrf_token = Mock()
+
+        csrf_token.return_value = csrf
+
+        request.session.get_csrf_token = csrf_token
+
+        return request
+
+    def _get_target_class(self):
 
         from deform_wizard import FormWizard
 
+        return FormWizard
+
+    def _make_one(self, request):
+
+        from deform_wizard import CSRFSchema
+
         import colander
 
-        class Name(colander.MappingSchema):
+        class Name(CSRFSchema):
 
             name = colander.SchemaNode(colander.String('UTF-8'))
 
-            title = colander.SchemaNode(colander.String('UTF-8'))
-
-            description = colander.SchemaNode(colander.String('UTF-8'))
-
-            first_name = colander.SchemaNode(colander.String('UTF-8'))
-
-            surname = colander.SchemaNode(colander.String('UTF-8'))
-
-        class Address (colander.MappingSchema):
-
-            name = colander.SchemaNode(colander.String('UTF-8'))
-
-            title = colander.SchemaNode(colander.String('UTF-8'))
-
-            description = colander.SchemaNode(colander.String('UTF-8'))
+        class Address (CSRFSchema):
 
             address_one = colander.SchemaNode(colander.String('UTF-8'))
 
             address_two = colander.SchemaNode(colander.String('UTF-8'))
 
-            town_city = colander.SchemaNode(colander.String('UTF-8'))
-
-            country = colander.SchemaNode(colander.String('UTF-8'))
-
-            post_code = colander.SchemaNode(colander.String('UTF-8'))
-
-        from pyramid.testing import DummyRequest
-
-        request = DummyRequest()
+        klass = self._get_target_class()
 
         name = Name().bind(request=request)
 
         address = Address().bind(request=request)
 
-        wiz = FormWizard('wizard', name, address)
+        return klass('wizard', name, address)
 
-        wiz(request)
+    def test_init_wizard(self):
+
+        wiz = self._make_one(self.request)
+
+        self.assertEqual(len(wiz.schemas), 2)
+
+    def test_render_wizard(self):
+
+        from deform_wizard import FormWizardView
+
+        wiz = self._make_one(self.request)
+
+        form = FormWizardView(wiz)(self.request)
+
+        self.assertIn('form', form.keys())
+
+    def test_validation_pass(self):
+
+        from deform_wizard import FormWizardView
+
+        request = self.get_csrf_request(post={'name': 'simon'})
+
+        wiz = self._make_one(request)
+
+        form = FormWizardView(wiz)(request)
+
+        self.assertNotIn('errorMsg', form['form'])
+
+    def test_validation_fail(self):
+
+        from deform_wizard import FormWizardView
+
+        request = self.get_csrf_request(post={'dave': 'nonsense'})
+
+        wiz = self._make_one(request)
+
+        form = FormWizardView(wiz)(request)
+
+        self.assertIn('errorMsg', form['form'])
+
+    def test_step_forwards(self):
+
+        from deform_wizard import FormWizardView
+
+        request = self.get_csrf_request(post={'name': 'simon', 'Next': 'next'})
+
+        wiz = self._make_one(request)
+
+        form = FormWizardView(wiz)(request)
+
+        self.assertIn('address_one', form['form'])
+
+    def test_step_backwards(self):
+
+        from deform_wizard import FormWizardView
+
+        request = self.get_csrf_request(post={'name': 'simon', 'Next': 'next'})
+
+        wiz = self._make_one(request)
+
+        form = FormWizardView(wiz)(request)
+
+        new_request = self.get_csrf_request(
+            post={'address_one': 'sasas',
+            'address_two': 'ewewe',
+            'Previous': 'previous'}
+            )
+
+        new_request.session = request.session
+
+        form = FormWizardView(wiz)(new_request)
+
+        self.assertIn('simon', form['form'])
+
+    def test_step_finish(self):
+
+        from deform_wizard import FormWizardView
+
+        request = self.get_csrf_request(post={'name': 'simon', 'Next': 'next'})
+
+        wiz = self._make_one(request)
+
+        form = FormWizardView(wiz)(request)
+
+        new_request = self.get_csrf_request(
+            post={'address_one': 'sasas',
+            'address_two': 'ewewe',
+            'Finish': 'finish'}
+            )
+
+        new_request.session = request.session
+
+        form = FormWizardView(wiz)(new_request)
+
+        self.assertEqual(2, len(form['wizard_data']))
